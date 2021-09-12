241b5de236f3f466042c6c05bc4f7333d49b52cf
RakaVaqaFlow <70295386+RakaVaqaFlow@users.noreply.github.com>
2021-09-12 19:00:09 +0300

    Update ex1.c

--- week4/ex1.c
+++ week4/ex1.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+int main(){
+    int n=1;
+    int v = fork();
+    if(v!=0)printf("Hello from parent [%d]\n", v-n);
+    else printf("Hello from child [%d]\n", -n);
+
+}
