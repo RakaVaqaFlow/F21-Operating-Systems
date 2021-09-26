b1877d2e0bf7b39e79e87a80fc7b44a8a2670180
RakaVaqaFlow <70295386+RakaVaqaFlow@users.noreply.github.com>
2021-09-27 01:19:43 +0300

    Add files via upload

--- week6/ex2.c
+++ week6/ex2.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE 10
+#define SIZE 100
+#define INF 1e9
 
 typedef struct process{
     int arrivalTime;
@@ -14,12 +15,14 @@ typedef struct process{
 } process;
 
 process p[SIZE];
+int used[SIZE];
 int n = 0;
 double averageTurnaroundTime=0;
 double averageWaitingTime=0;
 
 int cmp(const void* a, const void* b){
-    if( (*(process*)a).arrivalTime> (*(process*)b).arrivalTime ) return 1;
+    if( (*(process*)a).arrivalTime> (*(process*)b).arrivalTime 
+    || ( (*(process*)a).arrivalTime == (*(process*)b).arrivalTime && (*(process*)a).burstTime > (*(process*)b).burstTime) ) return 1;
     return 0;
 }
 
@@ -29,7 +32,6 @@ int maxVal(int a, int b){ //default max doesn't work correctly on my laptop ¯\_
 
 void print_info(){
     printf("P#  %5s %5s %5s %5s %5s\n", "AT", "BT", "CT", "TAT", "WT");
-    
     for(int i=0; i<n; i++){
         printf("P%-2d %5d %5d %5d %5d %5d\n", i+1, p[i].arrivalTime, p[i].burstTime, p[i].completionTime, p[i].turnAroundTime, p[i].waitingTime);
     }
@@ -46,25 +48,41 @@ void readFromFile(const char* s){
         n++;
     }
 }
+
 int main(){ 
+
     readFromFile("input.csv");
+    
 
-    //FCFS
+    //SJN
+    for(int i=0; i<n; i++) used[i]=0;
     qsort(p, n,sizeof(process), cmp);
-    int currentTime=0;
+    int currentTime=0, it=-1, minBT=INF;
     for(int i=0; i<n; i++){
-        currentTime = currentTime + p[i].burstTime + maxVal(p[i].arrivalTime - currentTime, 0);
-        p[i].completionTime = currentTime;
-        p[i].turnAroundTime = currentTime-p[i].arrivalTime;
-        p[i].waitingTime = p[i].turnAroundTime-p[i].burstTime;
+        it=-1, minBT=INF;
+        for(int j=0; j<n; j++){
+            if(!used[j] && currentTime>=p[j].arrivalTime && minBT>p[j].burstTime) {
+                it = j;
+                minBT = p[it].burstTime;
+            }
+        }
+        if(it=-1){
+            it=0;
+            while(used[it])it++;
+        }
+        used[it] = 1;
+        currentTime = currentTime + p[it].burstTime + maxVal(p[it].arrivalTime - currentTime, 0);
+        p[it].completionTime = currentTime;
+        p[it].turnAroundTime = currentTime-p[it].arrivalTime;
+        p[it].waitingTime = p[it].turnAroundTime-p[it].burstTime;
 
-        averageTurnaroundTime+=p[i].turnAroundTime;
-        averageWaitingTime+=p[i].waitingTime;
+        averageTurnaroundTime+=p[it].turnAroundTime;
+        averageWaitingTime+=p[it].waitingTime;
     }
-    //--
+    //---
     averageTurnaroundTime = averageTurnaroundTime/(n*1.0);
     averageWaitingTime = averageWaitingTime/(n*1.0);    
 
-    print_info();
-    
+    print_info(); 
 }
+
