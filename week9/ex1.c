a9f60b211db2e21c96653ac796ea6a5e20047bb3
VM Manjaro Vagif <lebronirish@gmail.com>
2021-10-17 00:31:38 +0300

    edit ex1.c

--- week9/ex1.c
+++ week9/ex1.c
@@ -1,1 +1,12 @@
+//Vagif Khalilov BS20-04
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/resource.h>
 
+int main(){
+   
+}
