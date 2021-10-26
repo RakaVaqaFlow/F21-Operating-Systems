c86494a34da83fafdc1141919b76d28962ca16ba
VM Manjaro Vagif <lebronirish@gmail.com>
2021-10-26 22:55:15 +0300

    week10

--- week10/ex3.sh
+++ week10/ex3.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+touch _ex3.txt
+echo "Vagif Khalilov" >> _ex3.txt
+chmod a-x _ex3.txt
+ls -l _ex3.txt > ex3.txt
+chmod 707 _ex3.txt
+ls -l _ex3.txt >> ex3.txt
+chmod g=u _ex3.txt
+ls -l _ex3.txt >> ex3.txt
+echo "Answers:">>ex3.txt
+chmod 660 _ex3.txt
+ls -l _ex3.txt >> ex3.txt
+echo "    So, 660 mean that only owner and group have can read file and write to it" >> ex3.txt
+echo >> ex3.txt
+chmod 775 _ex3.txt
+ls -l _ex3.txt >> ex3.txt
+echo "    So, 775 mean that only owner and group have all permissions, others can only read and execute it." >> ex3.txt
+echo >> ex3.txt
+chmod 777 _ex3.txt 
+ls -l _ex3.txt >> ex3.txt
+echo "    So, 777 mean that everyone can read, write and execute file" >> ex3.txt
---
+++ week10/ex4.c
@@ -0,0 +1,12 @@
+//Vagif Khalilov BS20-04
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <time.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <stdint.h>
+
+int main(){
+
+}
